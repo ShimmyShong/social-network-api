fd07f34ec8afd75721c8d2ce4e509ff477aa5be3
ShimmyShong <lattabun@gmail.com>
2023-07-08 11:46:37 -0400

    some boilerplate, adding models

---
+++ models/Reaction.js
@@ -0,0 +1,18 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    }
+)
---
+++ models/Thought.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(), //TODO: format this later
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    }
+)
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
